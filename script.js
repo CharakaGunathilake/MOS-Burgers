080862ecd15ce49737884892d2d4a3b394addeff
CharakaGunathilake <gunathilakec122@gmail.com>
2024-07-31 22:49:02 +0530

    finalized

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const btns = document.querySelectorAll(".menubtn");
 const translateAmount = 100;
 let translate = 0;
 
+
+
+
+
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll(".nav a").
     forEach(link => {
@@ -39,6 +43,7 @@ slide = (givenindex, pageIndex) => {
 }
 
 const foodItems = [".Burger", ".Submarine", ".Fries", ".Pasta", ".Chicken", ".Beverages"];
+const ar = [1, 3, 4, 6, 7, 9, 10, 12, 14, 15, 16, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 29, 31, 33, 34, 37, 40, 41, 42, 44];
 const menu = [
     burgers = [
         id = ["B1001", "B1002", "B1003", "B1004", "B1005", "B1006", "B1007", "B1008", "B1009", "B1010",
@@ -84,132 +89,105 @@ const menu = [
     ]
 ]
 
-let index = 0;
-if (activePage == "/orders.html") {
+loadMenu = () => {
     for (let j = 0; j < foodItems.length; j++) {
         const main = document.querySelector(foodItems[j]);
         let htmlString = "";
         for (let i = 0; i < menu[j][0].length; i++) {
-
-            index++;
-            htmlString += `<div class="card rounded-5 m-3">
-                                <div class="card-body d-flex">
-                                    <div class="w-50 d-flex align-items-center">
-                                        <div class="position-relative circle col-2">
-                                        </div>
-                                        <label class="me-5 fw-bold">${menu[j][0][i]} | ${menu[j][1][i]}</label>
-                                    </div>
-                                    <div class="d-flex align-items-center gap-3 p-3 border-start border-2 border-black">
-                                        <label class="bg-dark-subtle rounded-5 p-3 d-flex align-items-center">Rs. ${menu[j][2][i]} <div class="mx-2">
-                                        <button id="${index}" class="btn btn-dark rounded-5 ms-2" onclick = "showDiscount(${index})">di</button>
-                                    </div></label>
-                                        
-                                        <div
-                                            class="d-flex align-items-center p-2 gap-2 bg-danger rounded-5">
-                                            <div class="mx-2">
-                                                <input type="text" id="${menu[j][0][i]}" class="amountInput rounded-4" placeholder="00">
+            if (activePage == "/orders.html") {
+                index++;
+                htmlString += `<div class="card rounded-5 m-3">
+                                    <div class="card-body row">
+                                        <div class="col d-flex align-items-center">
+                                            <div class="position-relative circle col-2">
                                             </div>
-                                            <button class="btn btn-dark rounded-5" onclick = "add(${menu[j][0][i]},1)">+</button>
-                                            <div class="fw-bolder border-start border-2 border-black">
-                                                <button class="btn btn-dark rounded-5 ms-2" onclick = "sub(${menu[j][0][i]},1)">-</button>
-                                            </div>
+                                            <label class="me-5 fw-bold">${menu[j][0][i]} | ${menu[j][1][i]}</label>
                                         </div>
-                                        <button class="btn btn-success rounded-4 fw-bolder">Add to Cart</button>
+                                        <div class="col d-flex align-items-center gap-3 mx-5 border-start border-2 border-black">
+                                            <label class="bg-dark-subtle rounded-5 p-3 d-flex align-items-center">Rs. ${menu[j][2][i]} <div class="mx-2">
+                                            <button id="${index}" class="btn btn-dark rounded-5 ms-2" onclick = "showDiscount(${index})">di</button>
+                                        </div></label>
+                                            
+                                            <div
+                                                class="d-flex align-items-center p-2 gap-2 bg-danger rounded-5">
+                                                <div class="mx-2">
+                                                    <input type="text" id="${menu[j][0][i]}" class="amountInput rounded-4" placeholder="00">
+                                                </div>
+                                                <button class="btn btn-dark rounded-5" onclick = "add(${menu[j][0][i]},1)">+</button>
+                                                <div class="fw-bolder border-start border-2 border-black">
+                                                    <button class="btn btn-dark rounded-5 ms-2" onclick = "sub(${menu[j][0][i]},1)">-</button>
+                                                </div>
+                                            </div>
+                                            <button class="btn btn-success rounded-4 ms-2" onclick = "addCart('${menu[j][0][i]}','${menu[j][1][i]}','${menu[j][2][i]}')">Add To Cart</button>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>`
-        } main.innerHTML = htmlString;
-    }
-} else if (activePage == "/inventory.html") {
-    for (let j = 0; j < foodItems.length; j++) {
-        const main = document.querySelector(foodItems[j]);
-        let htmlString = "";
-        for (let i = 0; i < menu[j][0].length; i++) {
-            htmlString += `<div class="card rounded-5 m-3">
-                    <div class="card-body row">
-                        <div class="col d-flex align-items-center w-25">
-                            <div class="circle">
-                    
-                            </div>
-                            <label class="mx-3 fw-bold">${menu[j][0][i]} | ${menu[j][1][i]}</label>
+                                </div>`
+            } else if (activePage == "/inventory.html") {
+                htmlString += `<div class="card rounded-5 m-3">
+                <div class="card-body row">
+                    <div class="col d-flex align-items-center">
+                        <div class="position-position-absolute circle col-2">
                         </div>
-                        <div class="col d-flex align-items-center gap-3 mx-5" style="border-left: solid black 2px;">
-                            <label class="bg-dark-subtle rounded-5 p-3">
-                                Rs. ${menu[j][2][i]}</label>
-                    
-                            <div class="d-flex align-items-center p-2 gap-2 bg-danger rounded-5">
-                                <div class="mx-2 px-2 w-25">
-                                <input type="text" id="${menu[j][0][i]}" class="amountInput rounded-4" placeholder="00">
-                                </div>
-                                <button class="btn btn-dark rounded-5" onclick = "addi(${menu[j][0][i]},1)">+</button>
-                                <div class="border-start border-2 border-black">
-                                <button class="btn btn-dark rounded-5 mx-2" onclick = "subi(${menu[j][0][i]},1)"> - </button>
-                                </div>
-                            </div>
-                            <button class="btn btn-success rounded-5 fw-bolder px-4">Add To Inventory</button>
-                            <button class="btn btn-outline-danger rounded-4 fw-bolder">Expired</button>
-                            <button class="btn btn-outline-danger rounded-4 fw-bolder">Delete</button>
-                        </div>
+                        <label class="me-5 fw-bold">${menu[j][0][i]} | ${menu[j][1][i]}</label>
                     </div>
-                    </div>`
-        } main.innerHTML = htmlString;
+                    <div class="col d-flex align-items-center gap-3 mx-5 border-start border-2 border-black">
+                        <label class="bg-dark-subtle rounded-5 p-3 d-flex align-items-center">Rs. ${menu[j][2][i]}</label>
+                        
+                        <div
+                            class="d-flex align-items-center p-2 gap-2 bg-danger rounded-5">
+                            <div class="mx-2">
+                                <input type="text" id="${menu[j][0][i]}" class="amountInput rounded-4" placeholder="00">
+                            </div>
+                            <button class="btn btn-dark rounded-5" onclick = "add(${menu[j][0][i]},1)">+</button>
+                            <div class="fw-bolder border-start border-2 border-black">
+                                <button class="btn btn-dark rounded-5 ms-2" onclick = "sub(${menu[j][0][i]},1)">-</button>
+                            </div>
+                        </div>
+                        <button class="btn btn-outline-danger rounded-4 fw-bolder">Expired</button>
+                        <button class="btn btn-outline-danger rounded-4 fw-bolder">Delete</button>
+                    </div>
+                </div>
+            </div>`
+            } main.innerHTML = htmlString;
+        }
+    }
+    for (let i = 0; i < ar.length; i++) {
+        document.getElementById(ar[i]).style.display = 'none';
     }
 }
 
+let index = 0;
 
-const ar = [1, 3, 4, 6, 7, 9, 10, 12, 14, 15, 16, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 29, 31, 33, 34, 37, 40, 41, 42, 44];
-for (let i = 0; i < ar.length; i++) {
-    document.getElementById(ar[i]).style.display = 'none';
-}
+
+
+
 
 let currentId;
-let tot;
+let tot = 0;
 add = (id, amount) => {
     if (currentId != id) {
-        tot = amount;
-        id.value = tot;
         currentId = id;
+        tot = 1;
+        id.value = tot;
     } else {
         tot += amount;
         id.value = tot;
     }
 }
 sub = (id, amount) => {
-    if (tot == 0) {
+    if (id.value == 0) {
+
     } else {
         if (currentId != id) {
-            tot = 0;
-            id.value = tot;
             currentId = id;
-        } else {
-            tot -= amount;
-            id.value = tot;
+            tot = id.value;
         }
-    }
-}
-addi = (id, amount) => {
-    if (currentId != id) {
-        tot = amount;
+        tot -= amount;
         id.value = tot;
-        currentId = id;
-    } else {
-        tot += amount;
-        id.value = tot;
+
     }
 }
-subi = (id, amount) => {
-    if (tot == 0) {
-    } else {
-        if (currentId != id) {
-            tot = 0;
-            id.value = tot;
-            currentId = id;
-        } else {
-            tot -= amount;
-            id.value = tot;
-        }
-    }
-}
+
 
 const discountsArray = [
     discountId = [2, 5, 8, 11, 13, 17, 21, 30, 32, 35, 36, 38, 39, 43, 45, 46],
@@ -222,3 +200,36 @@ showDiscount = (index) => {
         }
     }
 }
+
+addCustomer = () => {
+    let phoneNumber = document.getElementById("customerNumber").value;
+    let customerName = document.getElementById("customerName").value;
+    let customerDiv = "";
+    let customerPage = document.querySelector(".customer");
+    customerDiv = `<div class="card rounded-5 m-3">
+    <div class="card-body row">
+            <label class="me-5 fw-bold">${phoneNumber} | ${customerName}</label>
+        </div>
+        <div class="col d-flex justify-content-center align-items-center gap-5 px-5 border-start border-2 border-black">
+            <button class="btn btn-success rounded-4 fw-bolder w-100">View</button>
+            <button class="btn btn-success rounded-4 fw-bolder w-100">Edit</button>
+            <button class="btn btn-success rounded-4 fw-bolder w-100">Delete</button>
+        </div>
+    </div>
+</div>`
+    customerPage.innerHTML += customerDiv;
+}
+
+addCart = (id,name,price) => {
+    alert("it's working",id);
+    let Cart = document.querySelector(".Cart");
+    let itemAmt = document.getElementById(id).value;
+    Cart.innerHTML += `<div class="card rounded-5 m-3 p-2">
+                    <div class="card-body d-flex justify-content-between">
+                            <label class="me-5 fw-bold">${id} | ${name} | ${price}</label> 
+                            <label class="me-5 fw-bold p-2 rounded-5 bg-danger text-white">X${itemAmt == 0 ? 1 : itemAmt}</label>
+                        </div>
+                    </div>
+                </div>`
+}
+
